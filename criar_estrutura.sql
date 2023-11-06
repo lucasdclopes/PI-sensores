224fcd8e8afc96ff2c400a51a2ca25a4e9da3f6c
Lucas de Carvalho Lopes <42218942+lucasdclopes@users.noreply.github.com>
2023-11-06 04:15:46 -0300

    .sql da estrutura do sistema

---
+++ criar_estrutura.sql
@@ -0,0 +1,43 @@
+create table medicao_sensor (
+	idMedicao int not null identity(1,1),
+	vlTemperatura decimal(6,2),
+	vlUmidade decimal (5,2),
+	dtMedicao datetime2(2) not null,
+	constraint PK__medicao_sensor primary key(idMedicao desc),
+	constraint UN_dtMedicao unique (dtMedicao desc)
+)
+
+drop table alerta_enviado
+drop table alerta
+go
+
+create table alerta (
+	idAlerta int not null identity(1,1),
+	isHabilitado bit not null,
+	tipoAlerta tinyint not null, --1 - TEMPERATURA. 2 - UMIDADE
+	intervaloEsperaSegundos smallint not null,
+	vlMax decimal(6,2) null, 
+	vlMin decimal(6,2) null,
+	dtCriado datetime2(2) not null,
+	dtUltimoEnvio datetime2(2) null,
+	destinatarios varchar(1000) not null,
+	constraint PK__alerta primary key (idAlerta desc)
+) 
+go
+
+create table alerta_enviado (
+	idEnviado int not null identity(1,1),
+	idAlerta int not null,
+	dtEnvio datetime2(2) not null,
+	constraint PK__alerta_enviado primary key (idEnviado desc),
+	constraint FK__alerta_enviado__idAlerta__alerta foreign key (idAlerta)
+		references alerta(idAlerta)
+)
+
+create table log_erros_sistema (
+	idLogErros int not null identity(1,1),
+	msgErro varchar(max) not null,
+	dtLog datetime2(2) not null,
+	stacktrace varchar(max) null,
+	constraint PK__log_erros_sistema primary key (idLogErros desc)
+)
